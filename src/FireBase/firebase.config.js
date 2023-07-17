2fbf8632c31d04b5fffbe185c4b3713b4e0181dd
shawon33 <iamcoming115@gmail.com>
2023-07-17 07:38:17 +0600

    Enviroment variable is done

--- src/FireBase/firebase.config.js
+++ src/FireBase/firebase.config.js
@@ -4,13 +4,20 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log(import.meta.env.VITE_apiKey);
 const firebaseConfig = {
-  apiKey: "AIzaSyC0zF3EFH2uegRxaeUri4Qx0ao-XNjOVDE",
-  authDomain: "toy-shop-421dc.firebaseapp.com",
-  projectId: "toy-shop-421dc",
-  storageBucket: "toy-shop-421dc.appspot.com",
-  messagingSenderId: "818294935022",
-  appId: "1:818294935022:web:f83b6a6b56769d735652ff"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env. VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
+  // apiKey: "AIzaSyC0zF3EFH2uegRxaeUri4Qx0ao-XNjOVDE",
+  // authDomain: "toy-shop-421dc.firebaseapp.com",
+  // projectId: "toy-shop-421dc",
+  // storageBucket: "toy-shop-421dc.appspot.com",
+  // messagingSenderId: "818294935022",
+  // appId: "1:818294935022:web:f83b6a6b56769d735652ff"
 };
 
 // Initialize Firebase
